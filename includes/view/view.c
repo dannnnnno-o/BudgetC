dc0e582c5031b0d45bf17f250364bdee401d5a4e
dannnnnno-o <dannnnnnnno.o@gmail.com>
2026-01-14 22:52:18 +0800

    added newlines

--- includes/view/view.c
+++ includes/view/view.c
@@ -143,10 +143,10 @@ void format_goal(Goal goal){
     }
     printf("Progress: %.2f%%\n\n", goal_progress);
 #elifdef __unix__
-    printf("%s: %sPHP", goal.name, amount);
-    printf("Invested Amount: %sPHP", goal.investment);
-    printf("Target Date: %s", goal.date);
-    printf("Progress: %.2f%%", goal_progress);
+    printf("%s: %sPHP\n", goal.name, amount);
+    printf("Invested Amount: %sPHP\n", goal.investment);
+    printf("Target Date: %s\n", goal.date);
+    printf("Progress: %.2f%%\n", goal_progress);
 #endif
 }
 
